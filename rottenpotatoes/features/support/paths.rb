e44b011d65cac1cb4322fd9864a2f27c9ec7c894
Stephen Goeddel <sgoeddel247@gmail.com>
2017-10-16 23:17:46 +0000

    completed first scenario

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -14,7 +14,10 @@ module NavigationHelpers
     case page_name
 
     when /^the home\s?page$/
-      '/'
+      '/movies'
+      
+    when /^the edit page for "(.*)"$/
+      edit_movie_path(Movie.where("title = ?", $1).first)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
